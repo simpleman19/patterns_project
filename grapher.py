e98a9ee24694ce35c8b0be8dbc77a10661b56544
Slidejiveman <rdnotlaw91@outlook.com>
2018-03-31 20:45:31 -0500

    Added class to grapher. Also added first static method so it now graphs the world population chart.

--- grapher.py
+++ grapher.py
@@ -1,1 +1,31 @@
-#main gui class
+from world import WorldVisualizer
+from tkinter import *
+
+
+class App:
+    """ Custom GUI class for creating graphs """
+
+    def __init__(self, master):
+        """ Should have a GUI element for each graph """
+
+        frame = Frame(master)
+        frame.pack()
+        # when specifying a command, do not specify arguments. Just give it the name.
+        # otherwise it will call the function on when declared. Not as an event.
+        world_b = Button(frame, text='World', command=graph_world)
+        world_b.grid(row=2, columnspan=2)
+
+
+def graph_world():
+    """ Class method: Creates a world population graph """
+
+    wv = WorldVisualizer()
+    wv.configure()
+    wv.style_render()
+    print("World Population graph from 2010 created.")
+
+
+root = Tk()
+root.wm_title('Speed Grapher')
+app = App(root)
+root.mainloop()
